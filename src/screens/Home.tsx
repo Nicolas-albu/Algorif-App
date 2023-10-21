01d46a198e5f5c2dea65f704aaecf4b835826ae9
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-10-21 14:21:56 -0300

    initial configurations

---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export function Home() {
+  return (
+    <View className="flex-row d-flex justify-center items-center flex-1">
+      <Text>Está funcionando Flávio!</Text>
+    </View>
+  )
+}
