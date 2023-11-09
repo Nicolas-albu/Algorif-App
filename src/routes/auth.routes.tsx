94510a31b2e77b8e593df28ffb3a4c770b3b1aca
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-11-09 13:46:09 -0300

    Added authentication and home routes

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,13 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { SignIn } from '@screens/SignIn'
+
+const Stack = createNativeStackNavigator()
+
+export function AuthRoutes() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="SignIn" component={SignIn} />
+    </Stack.Navigator>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { useEffect, useState } from 'react'
+import { User } from '@firebase/auth'
 
+import { AuthRoutes } from './auth.routes'
 import { TabRoutes } from './tab.routes'
+import { auth } from '@services/auth'
 
 export function Routes() {
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(setUser)
+
+    return subscriber
+  }, [])
+
   return (
     <NavigationContainer>
-      <TabRoutes />
+      {user ? <TabRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   )
 }
